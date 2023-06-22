73b9c9330c6e674d1fdf0a3659e820c65ac06b38
dinesh16a <dineshbabu.t2021csea@sece.ac.in>
2023-06-23 00:04:03 +0530

    final

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
+
+
 for(var i=0;i<document.querySelectorAll(".drum").length;i++){
-document.querySelectorAll("button")[i].addEventListener("click",hl);
-function hl(){
-    alert("done");
-}
+document.querySelectorAll("button")[i].addEventListener("click",function(){
+  var butt=this.innerHTML;
+    switch(butt){
+        case "w":
+            var audiow =new Audio("sounds/crash.mp3");
+            audiow.play();
+            break;
+        
+        case "a":
+            var audioa =new Audio("sounds/kick-bass.mp3");
+            audioa.play();
+            break;
+        
+        case "s":
+            var audio =new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "d":
+                var audio =new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+      
+        default:console.log.apply("error");        
+    }
+        
+});
 }
